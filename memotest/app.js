4b2df56d6f7f6fe0810b9d2afd1f7521f22cc94e
Facundo Nahuel Büchert <96147040+Facundo-Buchert@users.noreply.github.com>
2024-05-21 17:43:46 -0300

    Add files via upload

---
+++ memotest/app.js
@@ -0,0 +1,152 @@
+const textRound = document.querySelector("#round")
+const textStatus = document.querySelector("#status")
+let round = 0
+let foundPairs = 0
+
+document.querySelector("#size").onclick = function () {
+    startGame()
+}
+
+function startGame() {
+    resetGame()
+}
+
+function resetGame() {
+    resetRound()
+    resetStatus()
+    mixColors()
+    handleClicks()
+}
+
+function resetRound() {
+    round = 0
+    textRound.innerText = `Ronda Nº ${round}`
+}
+
+function resetStatus() {
+    foundPairs = 0
+    textStatus.innerText = `Aún no llevas ningún par encontrado... :(`
+}
+
+function convertToArray(colors) {
+    let array = []
+    for (let i = 0; i < colors.length; i++) {
+        array.push(colors[i])
+    }
+    return array
+}
+
+const board = document.querySelector("#board")
+let arrayColors
+
+function mixColors() {
+
+
+    const allColors = document.querySelectorAll(".color")
+    arrayColors = convertToArray(allColors)
+    arrayColors.sort(() => Math.random() - 0.5);
+    allColors.forEach(function (i) {
+        board.removeChild(i)
+    })
+    arrayColors.forEach(function (i) {
+        board.appendChild(i)
+    })
+
+    arrayColors.forEach(function (i) {
+        i.style.background = i.id
+    });
+    setTimeout(function () {
+        arrayColors.forEach(function (i) {
+            i.style.background = "grey"
+        });
+    }, 1250);
+
+}
+
+
+function handleClicks() {
+    let clicks = 0
+    arrayColors.forEach(function (color) {
+
+        color.onclick = function () {
+            if (color.style.background !== "white") {
+                if (color.className !== "color-1") {
+
+                    clicks++
+
+                    if (clicks === 1) {
+                        color.style.background = color.id
+                        color.classList = "color-1"
+                    }
+
+                    if (clicks === 2) {
+                        color.style.background = color.id
+                        color.classList = "color-2"
+
+                        const color1 = document.querySelector(".color-1")
+                        const color2 = document.querySelector(".color-2")
+
+                        if (color1.id === color2.id) {
+                            setTimeout(function () {
+                                color1.style.background = "white"
+                                color2.style.background = "white"
+                                color1.className = "color col-sm-4"
+                                color2.className = "color col-sm-4"
+                            }, 500)
+
+                            clicks = 0
+                            round++
+                            textRound.innerText = `Ronda Nº ${round}`
+                            foundPairs++
+                            if (foundPairs === 12) {
+                                textStatus.innerText = `Felicidades! Ya has encontrado los 12 pares! Pulsa COMENZAR para jugar de nuevo`
+
+                                const $alertCongratulations = document.createElement("div")
+                                $alertCongratulations.className = "alert alert-warning alert-dismissible fade show"
+                                $alertCongratulations.setAttribute("role", "alert")
+                                const $textCongratulations = document.createElement("strong")
+                                $textCongratulations.innerText = "¡MUY BIEN, GANASTE!"
+                                const $buttonClose = document.createElement("button")
+                                $buttonClose.type = "button"
+                                $buttonClose.className = "btn-close"
+                                $buttonClose.setAttribute("data-bs-dismiss", "alert")
+                                $buttonClose.setAttribute("aria-label", "Close")
+                                $alertCongratulations.appendChild($textCongratulations)
+                                $alertCongratulations.appendChild($buttonClose)
+                                const $containerInterface = document.querySelector("#container-interface")
+                                $containerInterface.appendChild($alertCongratulations)
+                                $buttonClose.onclick = function () {
+                                    $containerInterface.removeChild($alertCongratulations)
+                                }
+                                setTimeout(function () {
+                                    if ($alertCongratulations.parentNode === $containerInterface) {
+                                        $containerInterface.removeChild($alertCongratulations)
+                                    }
+                                }, 5000);
+                            }
+                            else if (foundPairs === 6) {
+                                textStatus.innerText = `Felicidades! Ya has econtrado la mitad de los pares, sigue así!`
+                            }
+                            else if (foundPairs === 1) {
+                                textStatus.innerText = `Recien llevas encontrado un par! Sigue así!`
+                            } else {
+                                textStatus.innerText = `Llevas encontrados ${foundPairs} pares! Sigue así!`
+                            }
+                        }
+                        else {
+                            setTimeout(function () {
+                                color1.style.background = "grey"
+                                color2.style.background = "grey"
+                                color1.className = "color col-sm-4"
+                                color2.className = "color col-sm-4"
+                            }, 500)
+                            clicks = 0
+                            round++
+                            textRound.innerText = `Ronda Nº ${round}`
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
